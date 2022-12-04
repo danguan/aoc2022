0ec66325aeff0e96e1ae675e1e5ba377639cfca1
Dan Guan <daniel.guan.bca@gmail.com>
2022-12-04 02:31:03 -0500

    Add day 4 input and solutions

---
+++ day4/solution1.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3.10
+import csv
+from typing import List
+
+
+class Solution(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def is_overlapped(
+        self, first_pair: List[str], second_pair: List[str]
+    ) -> bool:
+        """Returns whether first_pair/second_pair overlap entirely."""
+
+        first_ints = [int(s) for s in first_pair]
+        second_ints = [int(s) for s in second_pair]
+
+        return (
+            first_ints[0] <= second_ints[0] and first_ints[1] >= second_ints[1]
+        ) or (
+            second_ints[0] <= first_ints[0] and second_ints[1] >= first_ints[1]
+        )
+
+    def solve(self):
+        with open(self.filename) as f:
+            csv_reader = csv.reader(f)
+            overlap = 0
+
+            for row in csv_reader:
+                first_pair = row[0].split("-")
+                second_pair = row[1].split("-")
+
+                if self.is_overlapped(first_pair, second_pair):
+                    overlap += 1
+
+        print(overlap)
+
+
+if __name__ == "__main__":
+    sol = Solution("input.csv")
+    sol.solve()
